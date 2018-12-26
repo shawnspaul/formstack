c2ffd2906da87f1b107e47daa8c7b3c12ba1b48c
vagrant <vagrant@homestead>
2018-12-26 03:54:05 +0000

    fixing webhook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -10,12 +10,12 @@ class WebhookController extends Controller
 {
     public function submission(Request $request) {
         Log::info('INFO:'.json_encode($request->all()));
-        
+
         $submission = new Submissions();
-        $submission->name = $request->name;
-        $submission->email = $request->email;
-        $submission->prog_lang = $request->prog_lang;
-        $submission->tv_show = $request->tv_show;
+        $submission->name = $request->Name;
+        $submission->email = $request->Email;
+        $submission->prog_lang = $request->Preferred_Programming_Language;
+        $submission->tv_show = $request->What_is_your_favorite_TV_show;
         $submission->save();
     }
 }
