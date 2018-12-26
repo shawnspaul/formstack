c735dc4bea65ec08ceac7e1d871a16dbccd84db9
vagrant <vagrant@homestead>
2018-12-26 03:45:55 +0000

    fixing webhook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -9,12 +9,13 @@ use Illuminate\Support\Facades\Log;
 class WebhookController extends Controller
 {
     public function submission(Request $request) {
+        Log::info('INFO:'.json_encode($request->all()));
+        
         $submission = new Submissions();
         $submission->name = $request->name;
         $submission->email = $request->email;
         $submission->prog_lang = $request->prog_lang;
         $submission->tv_show = $request->tv_show;
         $submission->save();
-        Log::info('INFO:'.json_encode($request->all()));
     }
 }
